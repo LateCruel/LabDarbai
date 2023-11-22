533e35b0e801964f8c966a149876ef28f9e56cb6
LateCruel <dmitrijs.popovs@stud.vilniustech.lt>
2023-11-22 18:19:23 +0200

    Bootstrap svetaine #3

--- coffescript.js
+++ coffescript.js
@@ -1,24 +1,60 @@
+  /* Button to scroll to the top */
 document.addEventListener("DOMContentLoaded", function() {
-    var scrollButton = document.getElementById("scrollButton");
+  var scrollButton = document.getElementById("scrollButton");
+
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
+  };
+
+  scrollButton.onclick = function() {
+    scrollToTop();
+  };
+
+  function scrollToTop() {
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, currentScroll - currentScroll / 8);
+    }
+  }
+});
+  /* ------------------- */
+
+  /* Main section scroll */
+  function scrollToSection(event, sectionId) {
+    var targetSection = document.getElementById(sectionId);
+  
+    if (targetSection) {
+      event.preventDefault(); 
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+  /* ------------------- */
+
+  /* nav-container fiksacija */
+  document.addEventListener("DOMContentLoaded", function() {
+    var navContainer = document.querySelector('.nav-container');
+    var header = document.querySelector('.header');
+    var headerHeight = header.offsetHeight;
   
     window.onscroll = function() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollButton.style.display = "block";
+      var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
+  
+      if (scrollPosition > headerHeight) {
+        navContainer.style.position = 'fixed';
+        document.body.style.paddingTop = navContainer.offsetHeight + 'px';
       } else {
-        scrollButton.style.display = "none";
+        navContainer.style.position = 'static';
+        document.body.style.paddingTop = '0';
       }
     };
-  
-    scrollButton.onclick = function() {
-      scrollToTop();
-    };
-  
-    function scrollToTop() {
-      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  
-      if (currentScroll > 0) {
-        window.requestAnimationFrame(scrollToTop);
-        window.scrollTo(0, currentScroll - currentScroll / 8);
-      }
-    }
   });
+  /* ------------------- */
