249c6c81b70cf760b86b86d4463a9c05089b76b4
LateCruel <dmitrijs.popovs@stud.vilniustech.lt>
2023-11-29 01:03:10 +0200

    Bootstrap svetaine #9.2

--- coffescript.js
+++ coffescript.js
@@ -1,28 +1,28 @@
   /* Button to scroll to the top */
-document.addEventListener("DOMContentLoaded", function() {
-  var scrollButton = document.getElementById("scrollButton");
+  document.addEventListener("DOMContentLoaded", function() {
+    var scrollButton = document.getElementById("scrollButton");
 
-  window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollButton.style.display = "block";
-    } else {
-      scrollButton.style.display = "none";
+    window.onscroll = function() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+      } else {
+        scrollButton.style.display = "none";
+      }
+    };
+
+    scrollButton.onclick = function() {
+      scrollToTop();
+    };
+
+    function scrollToTop() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
     }
-  };
-
-  scrollButton.onclick = function() {
-    scrollToTop();
-  };
-
-  function scrollToTop() {
-    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-
-    if (currentScroll > 0) {
-      window.requestAnimationFrame(scrollToTop);
-      window.scrollTo(0, currentScroll - currentScroll / 8);
-    }
-  }
-});
+  });
   /* ------------------- */
 
   /* Main section scroll */
